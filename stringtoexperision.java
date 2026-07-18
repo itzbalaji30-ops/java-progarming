23858f834d2dd1af34e97b11a62b28fef8bfc849
itzbalaji30-ops <itzbalaji30@gmail.com>
2026-07-18 13:59:24 +0530

    add experision sum

--- stringtoexperision.java
+++ stringtoexperision.java
@@ -1,55 +1,22 @@
-import java.util.Stack;
-
-
 public class stringtoexperision {
     public static void main(String[] args) {
 
-        String s = "10*34+20/2-5";
+        String s = "2*3+4-3";
 
-        Stack<Integer> st = new Stack<>();
+        int ans = s.charAt(0) - '0';
 
-        int num = 0;
-        char op = '+';
+        for (int i = 1; i < s.length(); i += 2) {
+            char op = s.charAt(i);
+            int num = s.charAt(i + 1) - '0';
 
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            // Build the current number
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            }
-
-            // Process when an operator or the end of the string is reached
-            if (!Character.isDigit(ch) || i == s.length() - 1) {
-
-                switch (op) {
-                    case '+':
-                        st.push(num);
-                        break;
-
-                    case '-':
-                        st.push(-num);
-                        break;
-
-                    case '*':
-                        st.push(st.pop() * num);
-                        break;
-
-                    case '/':
-                        st.push(st.pop() / num);
-                        break;
-                }
-
-                op = ch;
-                num = 0;
-            }
-        }
-
-        int ans = 0;
-
-        while (!st.isEmpty()) {
-            ans += st.pop();
+            if (op == '+')
+                ans += num;
+            else if (op == '-')
+                ans -= num;
+            else if (op == '*')
+                ans *= num;
+            else if (op == '/')
+                ans /= num;
         }
 
         System.out.println(ans);
