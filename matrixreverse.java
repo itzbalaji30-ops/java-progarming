4a6ecd682404e724a3d58e9e1716a04a5d3d301c
itzbalaji30-ops <itzbalaji30@gmail.com>
2026-07-18 12:13:11 +0530

    Added matrix programs

--- matrixreverse.java
+++ matrixreverse.java
@@ -1,4 +1,15 @@
 public class matrixreverse {
     public static void main(String[] args) {
-    for
+          int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for(int row=0;row<3;row++){
+            for(int col=2;col>=0;col--){
+                System.out.print(matrix[row][col]+" ");
+            }
+            System.out.println();
+        }
+}
 }
