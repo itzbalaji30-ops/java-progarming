8754a41bdd1d910001438357c8f90856a683cd98
itzbalaji30-ops <itzbalaji30@gmail.com>
2026-07-21 15:26:56 +0530

    bank account

---
+++ bankaccount.java
@@ -0,0 +1,49 @@
+class bankacc {
+    private String accountNumber;
+    private double balance;
+
+    public bankacc(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+}
+public class bankaccount {  
+    public static void main(String[] args) {
+        bankacc account = new bankacc("123456789", 1000.0);
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Initial Balance: " + account.getBalance());
+
+        account.deposit(500.0);
+        System.out.println("Balance after deposit: " + account.getBalance());
+
+        account.withdraw(200.0);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+
+        account.withdraw(2000.0); // Invalid withdrawal
+    }
+    
+}
--- poly.java
+++ poly.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 class A{
     int add(int x, int y){
         return x+y;
