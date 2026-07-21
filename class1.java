4a1c4e8b6b0184adc8eddb6430290d8dfb797d22
itzbalaji30-ops <itzbalaji30@gmail.com>
2026-07-21 15:04:35 +0530

    polymorpisim

---
+++ class1.java
@@ -0,0 +1,16 @@
+class A{
+    int a=10;
+    int b=20;
+}
+class B {
+    int c=30;
+    int d=40;
+}
+public class class1 {
+    public static void main(String[] args) {
+        B b = new B();
+        A a = new A();
+        System.out.println(a.a+b.c);
+        
+    }
+}
---
+++ poly.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class A{
+    int add(int x, int y){
+        return x+y;
+    }
+    double add(double x, double y){
+        return x+y;
+    }
+    char add(char x, char y){
+        return (char)(x+y);
+    }
+}
+public class poly {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.add(5, 10));
+        System.out.println(a.add(5.5, 10.5));
+        System.out.println(a.add('A', 'B'));
+    }
+}
