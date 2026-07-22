f68240573309ff3cf75ae9101995bfb479cd29ef
itzbalaji30-ops <itzbalaji30@gmail.com>
2026-07-22 11:20:04 +0530

    exception

--- poly.java
+++ poly.java
@@ -10,11 +10,27 @@ class A{
         return (char)(x+y);
     }
 }
+class B extends A{
+@Override
+int add(int x, int y){ 
+        System.out.println("In class B trying to add two variables");
+        return x+y;
+    }
+@Override
+char add(char x, char y){
+        System.out.println("In class B trying to add two variables");
+        return (char)(x+y);
+    }
+
+}
 public class poly {
     public static void main(String[] args) {
         A a = new A();
         System.out.println(a.add(5, 10));
         System.out.println(a.add(5.5, 10.5));
         System.out.println(a.add('A', 'B'));
+        B b = new B();
+        b.add(5, 10);
+
     }
 }
